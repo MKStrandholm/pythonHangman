41bc26fe5495cb978551fb95342d50de6a7ef8ab
MKStrandholm <markkawucha@gmail.com>
2020-09-09 14:55:11 -0400

    Beta 0.1 push
    This build includes the win and lose conditions, proper blank replacement and 3 complete subject categories.
    
    KNOWN BUGS:
    - Duplicate letters are being replaced by others when a word has multiple instances.

--- Hangman.py
+++ Hangman.py
@@ -1,79 +1,214 @@
 import random
+from collections import Counter
 
+# Replay function. This is called when the win or lose condition is met to prompt the user to either replay or exit
+def replay():
+    print("\nWould you like to play again? 1) Yes 2) No")
+    validPlayAgain = False
+
+    # Input bool loop that controls the play again or exit choice
+    while (validPlayAgain == False):
+        # Get user input
+        playAgainChoice = input()
+        # If said input is a digit, cast it as such
+        if (playAgainChoice.isdigit()):
+            playAgainChoice = int(playAgainChoice)
+
+            # If the user entered 1, flip the game over bool flag which begins a new game
+            if (playAgainChoice == 1):
+                validPlayAgain = True
+                gameOver = False
+            # If the user entered 2, print a goodbye message and exit the program
+            elif (playAgainChoice == 2):
+                validPlayAgain = True
+                print("\nThank you for playing! Goodbye!")
+                exit()
+            # If the user entered anything else, provide an error message and reprompt
+            else:
+                print("Invalid choice. Please pick again.")
+        # If the user did not enter a digit, capitalize their input
+        else:
+            playAgainChoice = playAgainChoice.upper()
+            # If the user entered YES, flip the game over bool flag which begins a new game
+            if (playAgainChoice == "YES"):
+                validPlayAgain = True
+                gameOver = False
+            # If the user entered NO, print the goodbye message and exit the program
+            elif (playAgainChoice == "NO"):
+                validPlayAgain = True
+                print("Thank you for playing! Goodbye!")
+                exit()
+            # If the user entered anything else, provide an error message and reprompt
+            else:
+                print("Invalid choice. Please pick again.")
+
+''' Main game logic function. Takes in a list of words for easy future expansion; simply pass in a word
+    list from any of the other subject functions and this modular system will incorporate it seamlessly
+
+    NOTE: In the future, I plan on looking into reading from text files to pull in arrays externally instead of
+    manually hardcoding them '''
 def game(wordList):
-    lives = 5
+    # Setup of initial vars; 6 wrong guesses per word
+    lives = 6
+    # Word array that is populated with the passed in wordList
     words = []
+    # Guessed letters array to store which letters the user has already (incorrectly) guessed
+    guessedLetters = []
+    # Bool flag to track if a letter has already been guessed
+    alreadyGuessed = False
+    # Populate the words array with the passed in wordList
     for word in wordList:
         words.append(word)
-        
-    chosenWord = words[random.randint(0, 4)]
-    print("Your word has: " + str(len(chosenWord)) + " letters in it.")
+
+    # Amount of correct letters (used to track win condition)
+    correctLetters = 0
+
+    # Choose a random word from the word list
+    chosenWord = words[random.randint(0, len(words)-1)]
+    # Show the user how many letters are in the chosen word and print the appropriate amount of blanks
+    print("\nYour word has: " + str(len(chosenWord)) + " letters in it.")
     blanks = "_ " * len(chosenWord)
     print(blanks)
-    print("\n")
-    
+
+    # Bool flag to track letter input
     validLetter = False
+    # While the user has not yet entered a valid input
     while (validLetter == False):
-        if (lives >= 0):
-            letterInput = input("Guess a letter: ")
-            letterInput = letterInput.upper()
-            numCorrect = 0
-            index = 0
-            for i in chosenWord:
-                if (letterInput == i):
-                    # Guessed correctly
-                    numCorrect += 1
-                    index = chosenWord.index(letterInput)
-            if (numCorrect == 1):
-                print("There is " + str(numCorrect) + " " + letterInput + " in the word")
-                print(blanks)
-                blanks = blanks[:index * 2] + letterInput + blanks[(index * 2)+1:]
-                print(blanks)
-            elif (numCorrect > 1):
-                print("There are " + str(numCorrect) + " " + letterInput + "'s in the word")
-                blanks = blanks.replace(blanks[(index * 2)], letterInput)
+        # If the user has any lives remaining
+        if (lives > 0):
+            # If the amount of correctly guessed letters is equal to the length of the secret word
+            if (correctLetters == len(chosenWord)):
+                # The user has won, print a congratulatory message and flip the game over bool flag to end the game
+                print("\nYou won! Congratulations!")
+                validLetter = True
+                gameOver = True
+                # Prompt for replay
+                replay()
+            # Otherwise, if the game is still going on
             else:
-                print("There are no " + letterInput + "'s in the word. You have " + str(lives) + " lives remaining.")
-                lives -= 1
-        else:
-            validLetter = True
-            gameOver = True
-            print("\nGAME OVER!")    
-            print("Would you like to play again? 1) Yes 2) No")
-            validPlayAgain = False
-
-            while (validPlayAgain == False):
-                playAgainChoice = input()
-                if (playAgainChoice.isdigit()):
-                    playAgainChoice = int(playAgainChoice)
-
-                    if (playAgainChoice == 1):
-                        validPlayAgain = True
-                        gameOver = False
-                    elif (playAgainChoice == 2):
-                        validPlayAgain = True
-                        print("Thank you for playing! Goodbye!")
-                        exit()
-                    else:
-                        print("Invalid choice. Please pick again.")
+                # Prompt user for a letter
+                letterInput = input("\nGuess a letter: ")
+                # If user input is NOT an alphabetical character, or the input is greater than a single char
+                if (letterInput.isalpha() == False or len(letterInput) > 1):
+                    # Prompt the user with an error message
+                    print("Invalid input. Please only enter a single letter!")
+                # The user input is valid (alphabetical)
                 else:
-                    playAgainChoice = playAgainChoice.upper()
-                    if (playAgainChoice == "YES"):
-                        validPlayAgain = True
-                        gameOver = False
-                    elif (playAgainChoice == "NO"):
-                        validPlayAgain = True
-                        print("Thank you for playing! Goodbye!")
-                        exit()
+                    # Capitalize input
+                    letterInput = letterInput.upper()
+                    # If there are no letters in the guessed letter in the array
+                    if (len(guessedLetters) == 0):
+                        # Save this guess 
+                        guessedLetters.append(letterInput)
+                    # Otherwise, if there is atleast 1 or more guessed letters
                     else:
-                        print("Invalid choice. Please pick again.")
+                        # If the input is in the guessed letter array
+                        if (letterInput in guessedLetters):
+                            # Flip the bool flag to not reduce a life
+                            alreadyGuessed = True
+                            # Tell the user they've already guessed this letter
+                            print("You've already guessed this letter. Please try again.\n")
+                            print(blanks)
+                        # Otherwise, if the input is not yet in the guessed letter array
+                        else:
+                            # Flip the bool flag to reduce a life
+                            alreadyGuessed = False
+                            # Add the guessed letter to the guessed letter array
+                            guessedLetters.append(letterInput)
+
+                    # Set number of correct letters in the word to 0
+                    numCorrect = 0
+                    # Set an index value to 0 (to keep track of the letters' positions in the word)
+                    index = 0
+                    
+                    # Counter that detects duplicate letters in a word
+                    num = Counter(chosenWord)
+                    # For each letter in said word
+                    for i in chosenWord:
+                        # If a duplicate is detected
+                        if (num[i] > 1):
+                            # Create an array and enumerate through the word, storing the indices of each duplicate
+                            indices = [index for index, element in enumerate(chosenWord) if element == i]
+                            break
+                    
+                    # For each letter in the secret word
+                    for i in chosenWord:
+                        # If the inputted letter matches a letter in the word (guessed correctly)
+                        if (letterInput == i):
+                            # and if the player hasn't already guessed this letter
+                            if (alreadyGuessed == False):
+                                # Increment the number of correct letters for each instance of that letter in the word
+                                numCorrect += 1
+                                # Set the index value to the index of the input within the secret word
+                                index = chosenWord.index(letterInput)
+                    # If the user guessed a letter that is found once in the word
+                    if (numCorrect == 1):
+                        # Increment the correct letters (this is used to track win condition) 
+                        correctLetters += 1   
+                        # Tell them this
+                        print("\nThere is " + str(numCorrect) + " " + letterInput + " in the word.")
+                        ''' Update the blanks string, splicing in the guessed letter starting
+                             at the beginning of the string up to index * 2 (because of whitespaces) and ending
+                             at the char that's 1 index after that, until the end '''
+                        blanks = blanks[:index * 2] + letterInput + blanks[(index * 2)+1:]
+                        # Reprint the blanks with the updated letter(s)
+                        print("\n" + blanks)
+                    # If the user guessed a letter that is found more than once in the word
+                    elif (numCorrect > 1):
+                        # Increment the correct letters (this is used to track win condition) 
+                        correctLetters += numCorrect
+                        # Tell them this
+                        print("\nThere are " + str(numCorrect) + " " + letterInput + "'s in the word.")
+                        # For each element in the indices array created for duplicates
+                        for j in indices:
+                            # Splice in the letters at each index a duplicate was found
+                            blanks = blanks[:j * 2] + letterInput + blanks[(j * 2)+1:]
+                        # Reprint the blanks string
+                        print("\n" + blanks)
+                    # If the user guessed an incorrect letter
+                    else:
+                        # and if the user has not already guessed this letter
+                        if (alreadyGuessed == False):
+                            # Subtract a life
+                            lives -= 1
+                            # If the lives count is greater than 1 
+                            if (lives > 1):
+                                # Print a message using "lives" plural form
+                                print("\nThere are no " + letterInput + "'s in the word. You have " + str(lives) + " lives remaining.")
+                            # If the lives count is exactly 1
+                            elif  (lives == 1):
+                                # Print a message using "life" singular form
+                                print("\nThere are no " + letterInput + "'s in the word. You have " + str(lives) + " life remaining.")
+        # If the player has run out of lives
+        else:
+            # Flip bool flag to end the input loop
+            validLetter = True
+            # Flip bool flag to end the game
+            gameOver = True
+            # Print a game over message and show the user the word
+            print("\nGAME OVER! The word was: " + chosenWord + ".\n")
+            # Ask to replay 
+            replay()
                         
         
 def planets():
     # Planet word list
-    words = ['MERCURY', 'VENUS', 'EARTH', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
+    planetWords = ['MERCURY', 'VENUS', 'EARTH', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
     # Call the game function and pass in the above list
-    game(words)
+    game(planetWords)
+
+def colors():
+    # Color word list
+    colorWords = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'PURPLE', 'ORANGE', 'BLACK', 'WHITE', 'PINK', 'BROWN', 'GREY', 'CHARTREUSE', 'TURQUOISE', 'MAGENTA', 'TEAL', 'MAROON', 'BEIGE']
+    # Call the game function and pass in the above list
+    game(colorWords)
+
+
+def music():
+    # Music genre word list
+    musicWords = ['ROCK', 'METAL', 'POP', 'HIPHOP', 'TECHNO', 'RAP', 'BLUES', 'JAZZ', 'PUNK']
+    # Call the game function and pass in the above list
+    game(musicWords)
         
 # Program Start
 # Boolean to track game win/lose condition
@@ -81,7 +216,7 @@ gameOver = False
 
 # If the game is not yet finished, run the intro
 while (gameOver == False):
-    print("Welcome to Hangman! Pick a subject: 1) Planets 2) Video Games 3) Music")
+    print("\nWelcome to Hangman! Try to guess the word, but remember: you only have 6 lives! \nPick a subject: 1) Planets 2) Colors 3) Music")
     # A bool to control the input loop
     validChoice = False
 
@@ -98,10 +233,10 @@ while (gameOver == False):
             if (choice == 1):
                 validChoice = True
                 planets()
-            # Video Games
+            # Colors
             elif (choice == 2):
                 validChoice = True
-                videoGames()
+                colors()
             # Music
             elif (choice == 3):
                 validChoice = True
@@ -118,11 +253,11 @@ while (gameOver == False):
                 # End the loop and run planets function
                 validChoice = True
                 planets()
-            # If the choice was video games
-            elif (choice == "VIDEO GAMES"):
-                # End the loop and run video games function
+            # If the choice was colors
+            elif (choice == "COLORS"):
+                # End the loop and run colors function
                 validChoice = True
-                videoGames()
+                colors()
             # If the choice was music 
             elif (choice == "MUSIC"):
                 # End the loop and run music function
